a23314ef8a714a8ade7234c996416b02a3b36557
AadrikaJaiswal <iaadrikajaiswal@gmail.com>
2024-09-28 15:17:25 +0530

    code added for llama3

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ payload = {
 body = json.dumps(payload)
 
 # Set the model ID (with the correct parameter name 'modelId')
-model_id = "meta.llama2-70b-instruct-v1"
+model_id = "meta.llama3-70b-instruct-v1"
 
 # Invoke the model
 response = bedrock.invoke_model(
